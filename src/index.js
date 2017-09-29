e46c137c2881706910b5b37d059a20c3630455bd
Joseph Briggs <jbriggsr3@gmail.com>
2017-09-29 18:47:17 +0100

    Fixed issue regarding chartengine being undefined

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-export {BarChart} from './BarChart';
-export {ColumnChart} from './ColumnChart';
-export {LineChart} from './LineChart';
-export {PieChart} from './PieChart';
+import BarChart from './BarChart';
+import ColumnChart from './ColumnChart';
+import LineChart from './LineChart';
+import PieChart from './PieChart';
+
+export const chartengine = {
+  BarChart:BarChart,
+  ColumnChart:ColumnChart,
+  LineChart:LineChart,
+  PieChart:PieChart,
+}
