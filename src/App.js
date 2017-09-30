2086d40bbd4bde47efd41eba4d43470812c7340a
Joseph Briggs <jbriggsr3@gmail.com>
2017-09-30 17:20:09 +0100

    Fixed issue, where on uglify it would give a  error unknown t

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ const ngMaterial = require('angular-material');
 const ChartEngine = angular.module('chartEngine', ['ngMaterial']);
 const moment = require('moment');
 
-ChartEngine.config(function($interpolateProvider){
+ChartEngine.config(['$interpolateProvider', function($interpolateProvider){
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-});
+}]);
 
-ChartEngine.config(function($mdDateLocaleProvider) {
+ChartEngine.config(['$mdDateLocaleProvider', function($mdDateLocaleProvider) {
     $mdDateLocaleProvider.formatDate = function(date) {
        return moment(date).format('DD-MM-YYYY');
     };
-});
+}]);
 
 module.exports = ChartEngine;
